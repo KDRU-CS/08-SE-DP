5e27db450cbde1e7829e5a8e052eb0090ca5cf30
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2022-09-30 21:10:40 +0430

    adds Singleton Design Pattern

---
+++ src/SingletonDesignPattern/01/SingletonObj.cs
@@ -0,0 +1,19 @@
+﻿namespace SingletonDesignPattern._01;
+
+sealed class SingletonObj
+{
+    private static SingletonObj? uniqueSingleton;
+    private SingletonObj() { }
+    public static SingletonObj GetInstance()
+    {
+        if(uniqueSingleton is null)
+        {
+            uniqueSingleton = new SingletonObj();
+        }
+        return uniqueSingleton;
+    }
+    public void Print()
+    {
+        Console.WriteLine("Assalamu Alaikum!");
+    }
+}
---
+++ src/SingletonDesignPattern/02/SingletonObj.cs
@@ -0,0 +1,15 @@
+﻿namespace SingletonDesignPattern._02;
+
+sealed class SingletonObj
+{
+    private static SingletonObj uniqueSingleton = new SingletonObj();
+    private SingletonObj() { }
+    public static SingletonObj GetInstance()
+    {
+        return uniqueSingleton;
+    }
+    public void Print()
+    {
+        Console.WriteLine("Assalamu Alaikum!");
+    }
+}
